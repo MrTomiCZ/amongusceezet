5abcedf70b75c0be23c1921487fa89e0ba7d043a
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2025-08-28 11:26:01 +0200

    add fe for user

--- js/hry.js
+++ js/hry.js
@@ -25,16 +25,35 @@ hryRoot.innerHTML = `<div class="hra">Načítám hry…</div>`;
           });
         }
 
+        let hrac = {
+          id: "132456879012354",
+          username: "nepodarilosenacist",
+          discriminator: "0",
+          displayName: "Nepodařilo se načíst.",
+          joinedAt: Date.now(),
+          roles: [{id: "123156454566231", name: "Neexistuje"}]
+        };
+        if (g.user) {
+          try {
+            const res2 = await fetch("/getuserjson?who="+g.user, { headers: { "Accept": "application/json" } });
+            if (!res2.ok) throw new Error(`HTTP ${res2.status}`);
+            hrac = await res2.json();
+          } catch {
+
+          }
+        }
 
         return `
           <div class="hra">
             <div class="typ"><i class="fa-solid fa-gamepad"></i> ${g.gameType}</div>
             <div class="popis"><i class="fa-regular fa-comment"></i> ${g.gameDescription}</div>
             <div class="cas"><i class="fa-regular fa-clock"></i> ${cas}</div>
+            <div class="user"><i class="fa-regular fa-user"></i> ${hrac.displayName}</div>
           </div>
     `}).join("");
   } catch (err) {
     console.error(err);
-    hryRoot.innerHTML = `<div class="hra">Nepodařilo se načíst hry.</div>`;
+    hryRoot.innerHTML = `<div class="hra">Nepodařilo se načíst hry. Zkuste znovu načíst stránku (Ctrl+R nebo F5) pokud se nestalo automaticky.</div>`;
+    window.location.reload();
   }
 })();
